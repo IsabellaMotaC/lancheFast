edcf280f47d09ab5282bc86f162e005e2e7f556a
IsabellaMotaC <isabellamotacarvalho@hotmail.com>
2025-04-10 15:35:57 -0300

    Implementação do Edit, Create e Show do Produto

--- app/Livewire/Produto/ProdutoCreate.php
+++ app/Livewire/Produto/ProdutoCreate.php
@@ -2,10 +2,40 @@
 
 namespace App\Livewire\Produto;
 
+use App\Models\Produto;
 use Livewire\Component;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
 
 class ProdutoCreate extends Component
 {
+    use WithFileUploads;
+
+    public $nome, $ingredientes, $valor, $imagem;
+
+    protected $rules = [
+        'nome' => 'required|string|max:255',
+        'ingredientes' => 'required|string',
+        'valor' => 'required|numeric|min:0',
+        'imagem' => 'nullable|image|max:2048'
+    ];
+
+    public function store()
+    {
+        $this->validate();
+
+        $path = $this->imagem ? $this->imagem->store('produtos', 'public') : null;
+
+        Produto::create([
+            'nome' => $this->nome,
+            'ingredientes' => $this->ingredientes,
+            'valor' => $this->valor,
+            'imagem' => $path
+        ]);
+
+        session()->flash('success', 'Produto cadastrado com sucesso!');
+        return redirect()->route('produtos.index');
+    }
+
     public function render()
     {
         return view('livewire.produto.produto-create');
